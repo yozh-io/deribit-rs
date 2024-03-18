bd55f9880f9bc25a14814b76acb07ce7935a459b
MykhailoRomanenko <moromanenko@gmail.com>
2024-03-18 17:17:29 +0200

    remove broken fields from user orders data

--- src/models/subscription/channels/user_orders.rs
+++ src/models/subscription/channels/user_orders.rs
@@ -11,7 +11,7 @@ pub struct UserOrdersData {
     pub amount: f64,
     pub api: bool,
     pub average_price: f64,
-    pub commission: f64,
+    // pub commission: f64,
     pub creation_timestamp: u64,
     pub direction: Direction,
     pub filled_amount: f64,
@@ -26,7 +26,7 @@ pub struct UserOrdersData {
     pub order_type: OrderType,
     pub post_only: bool,
     pub price: f64,
-    pub profit_loss: f64,
+    // pub profit_loss: f64,
     pub reduce_only: bool,
     pub stop_price: Option<f64>,
     pub time_in_force: TimeInForce,
@@ -34,7 +34,7 @@ pub struct UserOrdersData {
     pub triggered: Option<bool>,
     pub usd: Option<f64>,
     pub replaced: bool, // TODO: Remove the Option when necessary
-    pub web: bool,
+    pub web: Option<bool>,
 }
 
 #[derive(Debug, Clone)]
