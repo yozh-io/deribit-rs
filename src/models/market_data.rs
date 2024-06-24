da0abe395e263a8859475a1f53103a32ace39a12
Mykhailo Romanenko <moromanenko@gmail.com>
2024-06-24 20:02:35 +0300

    add price index field to instrument

--- src/models/market_data.rs
+++ src/models/market_data.rs
@@ -167,6 +167,7 @@ pub struct GetInstrumentsResponse {
     pub strike: Option<f64>,
     pub tick_size: f64,
     pub max_leverage: u32,
+    pub price_index: String,
 }
 
 impl Request for GetInstrumentsRequest {
